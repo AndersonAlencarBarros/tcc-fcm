f2c423c4542805a7db6a9a864b5c0250b49535b7
Anderson Alencar <alencarbarros@protonmail.com>
2023-07-04 21:47:29 -0300

    plot por dimensao ou por obs

--- src/plot.py
+++ src/plot.py
@@ -1,106 +1,131 @@
-import numpy as np
 import pandas as pd
 from mpmath import *
-import math
-  
-  
-quantidade_de_bases = 3
-quantidade_de_observacoes = [10, 30, 100]
-dimensoes = [4, 8, 16]
-expoentes_fuzzy = [1.1, 8.325, 15.550, 22.775, 30.0]
+from parametros import Parametros
 
+
+p = Parametros()
+    
 
 """
-Lê os resultados e escolhe o Hullermeier de menor custo
-
-Plota os resultados
+Script para plotar os resultados
 """
 
 resultado = {
-    "obs_10": {
-        "dimensao_4": [],
-        "dimensao_8": [],
-        "dimensao_16": []
+    "10 observações": {
+        "4 dimensões": [],
+        "8 dimensões": [],
+        "16 dimensões": []
     }, 
-    "obs_30": {
-        "dimensao_4": [],
-        "dimensao_8": [],
-        "dimensao_16": []
+    "30 observações": {
+        "4 dimensões": [],
+        "8 dimensões": [],
+        "16 dimensões": []
     }, 
-    "obs_100": {
-        "dimensao_4": [],
-        "dimensao_8": [],
-        "dimensao_16": []
+    "100 observações": {
+        "4 dimensões": [],
+        "8 dimensões": [],
+        "16 dimensões": []
     }, 
 }
 
-for obs in quantidade_de_observacoes:
-    for dimensao in dimensoes:
-        for expoente in expoentes_fuzzy:
-            nome_pasta: str = f"resultados/obs_{obs}/experimento_dimensao_{dimensao}_obs_{obs}_expoente_fuzzy_{expoente}/"
+resultado_por_dimensao = {
+    "4 dimensões": {
+        "10 observações": [],
+        "30 observações": [],
+        "100 observações": [],
+    },
+    "8 dimensões": {
+        "10 observações": [],
+        "30 observações": [],
+        "100 observações": [],
+    },
+    "16 dimensões": {
+        "10 observações": [],
+        "30 observações": [],
+        "100 observações": [],
+    },
+}
 
-            menor_custo = mpf(math.inf)
-            hullermeier_menor_custo = mpf(0)
-            for i in range(quantidade_de_bases):
-                nome_arquivo: str = f"{nome_pasta}/experimento_inicializacao_{i}.csv"
-                df = pd.read_csv(nome_arquivo, dtype=str)
-                                
-                custo_atual = mpf(df["custo"][0])                               
-                hullermeier_atual = mpf(df["hullermeier"][0]) 
-                if custo_atual < menor_custo:
-                    menor_custo = custo_atual
-                    hullermeier_menor_custo = hullermeier_atual
+"""
+Preenche os dicionários
+"""
+
+for obs in p.quantidade_de_observacoes:
+    for dimensao in p.dimensoes:
+        for expoente in p.expoentes_fuzzy:
+            nome_arquivo: str = f"resultado_agregado/obs_{obs}/experimento_dimensao_{dimensao}_expoente_fuzzy_{expoente}.csv"
+            df = pd.read_csv(nome_arquivo, dtype=str)
             
-            resultado[f'obs_{obs}'][f"dimensao_{dimensao}"].append(hullermeier_menor_custo)
+            media_de_hullermeier = mpf(df["Média do Indice de Hullermeier"][0]) 
+            
+            resultado[f'{obs} observações'][f"{dimensao} dimensões"].append(media_de_hullermeier)
+            resultado_por_dimensao[f"{dimensao} dimensões"][f'{obs} observações'].append(media_de_hullermeier)
+
+"""                
+Exibe os Resultados
+"""
                 
-
-from pprint import pprint
-pprint(resultado)
-
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Define os valores de x
-x_values = [1.1, 8.325, 15.55, 22.775, 30.0]
+# Criar gráficos para cada dimensão ou por quantidade de observacoes
 
-# Obtém as chaves do dicionário
-obs_keys = list(resultado.keys())
+for dimensao, observacoes in resultado.items():
+    plt.figure(figsize=(8, 6))
+    plt.title(f"{dimensao}")
+    plt.xlabel("Expoente Fuzzy")
+    plt.ylabel("Índice de Hullermeier")
 
-# Define as dimensões para plotagem
-dimensoes = ['dimensao_4', 'dimensao_8', 'dimensao_16']
+    for observacao, valores in observacoes.items():
+        plt.plot(p.expoentes_fuzzy, valores, label=observacao)
 
-# Configura a precisão dos números
-mp.dps = 25
+    plt.legend()
+    plt.grid()
+    plt.show()
 
-# Loop pelos valores de observação (obs_10, obs_30, obs_100)
-for i, obs_key in enumerate(obs_keys):
-    obs_data = resultado[obs_key]
+
+"""
+Gráfico Geral, com todas as visualizações.
+"""
+
+
+# # Obtém as chaves do dicionário
+# obs_keys = list(resultado.keys())
+
+# print(obs_keys)
+
+# # Define as dimensões para plotagem
+# dimensoes = ['4 dimensões', '8 dimensões', '16 dimensões']
+
+# # Configura a precisão dos números
+# mp.dps = 25
+
+# for i, obs_key in enumerate(obs_keys):
+#     obs_data = resultado[obs_key]
     
-    # Loop pelas dimensões (dimensao_4, dimensao_8, dimensao_16)
-    for j, dim_key in enumerate(dimensoes):
-        dim_data = obs_data[dim_key]
+#     for j, dim_key in enumerate(dimensoes):
+#         dim_data = obs_data[dim_key]
         
-        # Converte os valores mpmath em float para plotagem
-        y_values = [float(nstr(value)) for value in dim_data]
+#         y_values = [float(nstr(value)) for value in dim_data]
         
-        # Cria um novo subplot para cada combinação de obs e dimensão
-        plt.subplot(3, 3, i * 3 + j + 1)
+#         # Cria um novo subplot para cada combinação de obs e dimensão
+#         plt.subplot(3, 3, i * 3 + j + 1)
         
-        sns.set_style("whitegrid")
-        sns.scatterplot(x=x_values, y=y_values, )
-        sns.lineplot(x=x_values, y=y_values,)
+#         sns.set_style("whitegrid")
+#         sns.scatterplot(x=p.expoentes_fuzzy, y=y_values, )
+#         sns.lineplot(x=p.expoentes_fuzzy, y=y_values,)
         
-        plt.title(f'{obs_key}, {dim_key}')
-        plt.xlabel('Expoente Fuzzy')
-        plt.ylabel('Hullemeier')
+#         plt.title(f'{obs_key}, {dim_key}')
+#         plt.xlabel('Expoente Fuzzy')
+#         plt.ylabel('Hullemeier')
         
-        plt.ylim(0.45, 1.05)
-        plt.xlim(1.1, 30)
+#         plt.ylim(0.45, 1.05)
+#         plt.xlim(1.1, 30)
         
-        plt.xticks(fontsize=10)
-        plt.yticks(fontsize=10)
-        plt.grid(True, linestyle='--', alpha=0.5)
+#         plt.xticks(fontsize=10)
+#         plt.yticks(fontsize=10)
+#         plt.grid(True, linestyle='--', alpha=0.5)
 
 
-plt.tight_layout()
-plt.show() 
+# plt.tight_layout()
+# plt.show() 
